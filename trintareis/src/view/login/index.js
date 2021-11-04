2f1ecffc0236aa90fbc3bfce12e6c615df8949ee
marcos <marcos.souza.anl@gmail.com>
2021-11-04 09:05:10 -0300

    ajustes

--- trintareis/src/view/login/index.js
+++ trintareis/src/view/login/index.js
@@ -9,25 +9,27 @@ function Login(){
                     <h1 className="h1 mb-3 text-info font-weight-bold">30Reis</h1>
                 </div>
 
-                <input type="email" class="form-control my-2" placeholder="E-mail"/>
-                <input type="password" class="form-control my-2" placeholder="Senha"/>        
+                <input type="email" class="form-control my-2" placeholder="E-mail*"/>
+                <input type="password" class="form-control my-2" placeholder="Senha*"/>
+                
+                <div class="col-12 text-right mb-8">
+                    <a href="/esqueci-minha-senha" class="color-primary font-7 text-right mb-24">Esqueci minha senha</a>
+                </div>
 
                 <button className="w-100 btn btn-lg btn-primary btn-login" type="submit">Entrar</button>
                 
-                <div className="msg-login text-dark text-center my-5">
+                {/* <div className="msg-login text-dark text-center my-5">
                     <span><strong>WoW!</strong> Você está conectado! &#128526;</span>
                     <br></br>
                     <span><strong>Ops!</strong> Verifique a senha ou usuário! &#128546;</span>
-                </div>
-                <div className="opcoes-login mt-1 text-center">
-                    <a href="#" className="mx-2">Recuperar senha</a>
-                    <span className="mx-2">&#9733;</span>
-                    <a href="#" className="mx-2">Cadastrar</a>
+                </div> */}
+                <div class="auth-form__account text-center mt-24 mb-28">Não tem uma conta?
+                    <a class="auth-form__account--link false" href="/cadastrar/2f" id="create-account-button" tabindex="4">  Crie uma nova conta</a>
                 </div>
 
-                <button className="signinPrivacy mt-50" type="button">
+                {/* <button className="signinPrivacy mt-50" type="button">
                     Termo de uso | Política de Privacidade
-                </button>
+                </button> */}
             </form>
         </div>
     )
