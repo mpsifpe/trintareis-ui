bc59343b7e4da85cbea31eccebc4618b2c7d1458
marcos <marcos.souza.anl@gmail.com>
2021-11-04 07:49:53 -0300

    ajustes

--- trintareis/src/view/login/index.js
+++ trintareis/src/view/login/index.js
@@ -6,21 +6,27 @@ function Login(){
         <div className="login-content d-flex align-items-center">
             <form className="form-signin mx-auto">
                 <div className="text-center mb-4">
-                    <img className="mb-4" src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"/>
-                    <h1 className="h3 mb-3 fw-normal">Login</h1>
+                    {/* <img className="mb-4" src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"/> */}
+                    <h1 className="h1 mb-3 text-white font-weight-bold">30Reis</h1>
                 </div>
 
-                <div className="form-label-group">
-                    <input type="email" class="form-control" placeholder="E-mail"/>
+                <input type="email" class="form-control my-2" placeholder="E-mail"/>
+                <input type="password" class="form-control my-2" placeholder="Senha"/>        
+
+                <button className="w-100 btn btn-lg btn-primary btn-login" type="submit">Entrar</button>
+                
+                {/* <div className="msg-login text-white text-center my-5">
+                    <span><strong>WoW!</strong> Você está conectado! &#128526;</span>
+                    <br></br>
+                    <span><strong>Ops!</strong> Verifique a senha ou usuário! &#128546;</span>
+                </div> */}
+                <div className="opcoes-login mt-1 text-center">
+                    <a href="#" className="mx-2">Recuperar senha</a>
+                    <span className="text-white">&#9733;</span>
+                    <a href="#" className="mx-2">Cadastrar</a>
                 </div>
-                <div className="form-label-group">
-                    <input type="password" class="form-control" placeholder="Senha"/>
-                </div>        
-                <div className="form-label-sign-in">
-                    <button className="w-100 btn btn-lg btn-primary" type="submit">Entrar</button>
-                    {/* <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p> */}
-                </div>
-                <button className="signinPrivacy" type="button">
+
+                <button className="signinPrivacy mt-50" type="button">
                     Termo de uso | Política de Privacidade
                 </button>
             </form>
