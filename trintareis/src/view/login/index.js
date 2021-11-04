7fbc74305b15f7056aa9675010d43b9b04a58ddf
marcos <marcos.souza.anl@gmail.com>
2021-11-04 08:18:22 -0300

    ajustes

--- trintareis/src/view/login/index.js
+++ trintareis/src/view/login/index.js
@@ -3,11 +3,10 @@ import './login.css'
 
 function Login(){
     return(
-        <div className="login-content d-flex align-items-center">
+        <div className="container-fluid d-flex justify-content-between align-items-center w-100">
             <form className="form-signin mx-auto">
                 <div className="text-center mb-4">
-                    {/* <img className="mb-4" src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"/> */}
-                    <h1 className="h1 mb-3 text-white font-weight-bold">30Reis</h1>
+                    <h1 className="h1 mb-3 text-info font-weight-bold">30Reis</h1>
                 </div>
 
                 <input type="email" class="form-control my-2" placeholder="E-mail"/>
@@ -15,14 +14,14 @@ function Login(){
 
                 <button className="w-100 btn btn-lg btn-primary btn-login" type="submit">Entrar</button>
                 
-                {/* <div className="msg-login text-white text-center my-5">
+                <div className="msg-login text-dark text-center my-5">
                     <span><strong>WoW!</strong> Você está conectado! &#128526;</span>
                     <br></br>
                     <span><strong>Ops!</strong> Verifique a senha ou usuário! &#128546;</span>
-                </div> */}
+                </div>
                 <div className="opcoes-login mt-1 text-center">
                     <a href="#" className="mx-2">Recuperar senha</a>
-                    <span className="text-white">&#9733;</span>
+                    <span className="mx-2">&#9733;</span>
                     <a href="#" className="mx-2">Cadastrar</a>
                 </div>
 
