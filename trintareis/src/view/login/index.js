d082a32d5b700bb6311f60b36206ac58e345be44
marcos <marcos.souza.anl@gmail.com>
2021-12-02 20:10:59 -0300

    ajuste no hover dos button

--- trintareis/src/view/login/index.js
+++ trintareis/src/view/login/index.js
@@ -27,18 +27,6 @@ function Login() {
                             <div class="col-12 text-end mb-8">
                                 <a href="#" class="color-primary font-7 mb-24">Esqueci minha senha</a>
                             </div>
-
-                            {/* <div className="msg-login text-dark text-center my-5">
-                                    <span><strong>WoW!</strong> Você está conectado! &#128526;</span>
-                                    <br></br>
-                                    <span><strong>Ops!</strong> Verifique a senha ou usuário! &#128546;</span>
-                                </div>
-                            <div class="auth-form__account text-center mt-24 mb-28">Não tem uma conta?
-                                    <a class="auth-form__account--link false" href="/cadastrar/2f" id="create-account-button" tabindex="4">  Crie uma conta</a>
-                                </div>
-                            <button className="signinPrivacy mt-50" type="button">
-                                Termo de uso | Política de Privacidade
-                                </button> */}
                         </form>
                     </div>
                     <div className="div-create-account mx-auto">
