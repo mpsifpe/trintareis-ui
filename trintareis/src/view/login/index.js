611913ff91038c484a3acf24ec69244f373cc006
marcos <marcos.souza.anl@gmail.com>
2021-12-03 18:20:39 -0300

    integração com firebase

--- trintareis/src/view/login/index.js
+++ trintareis/src/view/login/index.js
@@ -1,7 +1,19 @@
-import React from "react";
-import './login.css'
+import React, { useState } from 'react';
+import './login.css';
+import firebase from '../../config/firebase';
+import 'firebase/auth';
 
 function Login() {
+    const [email, setEmail] = useState();
+    const [senha, setSenha] = useState();
+
+    function singIn() {
+        firebase.auth().signInWithEmailAndPassword(email, senha).then(resultado => {
+            alert('Usuário logado!');
+        }).catch(erro => {
+            alert(erro);
+        })
+    }
     return (
         <div className="div-main">
             <nav className="navbar">
@@ -9,35 +21,33 @@ function Login() {
                     <a href="#">&#9818;30Reis</a>
                 </div>
             </nav>
-            <main>
-                <div className="div-container container-fluid d-flex justify-content-between align-items-center w-100">
-                    <div className="form-signin mx-auto">
-                        <form className="signin-container__form">
-                            <h1 class="title">Já têm uma conta?</h1>
-                            <p className="subtitle">Faça seu login e interaja com milhares de pessoas!</p>
-                            <fieldset className="textfield mb-24 mt32">
+            <div className="div-container container-fluid d-flex justify-content-between align-items-center w-100">
+                <div className="form-signin mx-auto">
+                    <form className="signin-container__form">
+                        <h1 class="title">Já têm uma conta?</h1>
+                        <p className="subtitle">Faça seu login e interaja com milhares de pessoas!</p>
+                        <fieldset className="textfield mb-24 mt32">
                                 <label>E-mail</label>
-                                <input type="email" class="form-control my-2" placeholder="Digite seu e-mail" />
+                                <input onChange={(e) => setEmail(e.target.value)} type="email" class="form-control my-2" placeholder="Digite seu e-mail" />
                             </fieldset>
                             <fieldset className="textfield mb-12">
                                 <label>Senha</label>
-                                <input type="password" class="form-control my-2" placeholder="Digite sua Senha"/>
+                                <input onChange={(e) => setSenha(e.target.value)} type="password" class="form-control my-2" placeholder="Digite sua Senha"/>
                             </fieldset>
-                            <button className="w-100 btn btn-lg fw-bold" type="submit">Entrar</button>
-                            <div class="col-12 text-end mb-8">
-                                <a href="#" class="color-primary font-7 mb-24">Esqueci minha senha</a>
-                            </div>
-                        </form>
-                    </div>
-                    <div className="div-create-account mx-auto">
-                        <h2>Criar uma conta</h2>
-                        <h2>é rápido, fácil</h2>
-                        <h2>e gratuito!!</h2>
-                        <h2>Faça agora mesmo &#128521;</h2>
-                        <button className="w-100 btn btn-lg fw-bold" type="submit">Criar minha conta</button>
-                    </div>
+                        <button onClick={singIn} className="w-100 btn btn-lg fw-bold" type="button">Entrar</button>
+                        <div class="col-12 text-end mb-8">
+                            <a href="#" class="color-primary font-7 mb-24">Esqueci minha senha</a>
+                        </div>
+                    </form>
                 </div>
-            </main>
+                <div className="div-create-account mx-auto">
+                    <h2>Criar uma conta</h2>
+                    <h2>é rápido, fácil</h2>
+                    <h2>e gratuito!!</h2>
+                    <h2>Faça agora mesmo &#128521;</h2>
+                    <button className="w-100 btn btn-lg fw-bold" type="submit">Criar minha conta</button>
+                </div>
+            </div>
         </div>
     )
 }
