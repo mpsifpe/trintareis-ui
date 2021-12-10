6bf87807c164eb172821159e8911c01105469e45
marcos <marcos.souza.anl@gmail.com>
2021-12-10 15:03:30 -0300

    criando feedPost e feedContent

--- trintareis/src/components/header/index.js
+++ trintareis/src/components/header/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import { FaHome, FaRocketchat, FaEarlybirds, FaUserFriends, FaUniversity, FaSearch, FaPlus } from "react-icons/fa";
+import { FaHome, FaRocketchat, FaEarlybirds, FaUserFriends, FaUniversity, FaSearch } from "react-icons/fa";
 import { IoIosNotifications, IoIosSchool } from "react-icons/io";
 import { MdOutlineGroups } from "react-icons/md";
+import { AiFillVideoCamera, AiFillPicture } from "react-icons/ai";
+import { BsCalendarDate } from "react-icons/bs";
+
+import Stories from '../../components/stories/index';
+import minios_bg from '../../resources/minios.jpg';
 
 import './header.css';
 import { Link } from 'react-router-dom';
@@ -45,6 +50,42 @@ function Header() {
                     </div>
                 </div>
             </div>
+
+            {/* <Stories/> */}
+
+            <div className="feed">
+                <div className="feedForm">
+                    <img src={minios_bg} />
+                    <input type="text" placeholder="Começar Publicar" />
+
+                    <div className="feedIcons">
+                        <div className="iconSingle img">
+                            <AiFillPicture />
+                            <span>Foto</span>
+                        </div>
+                        <div className="iconSingle">
+                            <AiFillVideoCamera />
+                            <span>Vídeo</span>
+                        </div>
+                        <div className="iconSingle evn">
+                            <BsCalendarDate />
+                            <span>Evento</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="feedPost">
+                    <div className="feedPostSingle">
+                        <div className="feedPost__profile">
+                            <img src={minios_bg} />
+                            <h3>TrintaReis<br/><span>20:00</span></h3>
+                        </div>
+                        <div className="feedPost__content">
+                            <p>Fazer parte do Trinta Reis significa ser do time vencedor!!</p>
+                            <img src="https://pbs.twimg.com/media/EKkDakiXsAECxgW.jpg" />
+                        </div>
+                    </div>
+                </div>
         </div>
     )
 }
