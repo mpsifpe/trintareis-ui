a0b8c6a48bbbda2a662eddbc45b2a4e3e50eb3e0
Jonathan Martins <jcam1@discente.ifpe.edu.br>
2022-05-14 16:15:29 -0300

    carregamento de contatos funcionando

--- trintareis/src/components/friend-card/index.js
+++ trintareis/src/components/friend-card/index.js
@@ -1,49 +1,46 @@
 import './friend-card.css';
 import { Link } from "react-router-dom";
-import React, { useState } from 'react';
+import React from 'react';
+import { useStateIfMounted } from 'use-state-if-mounted';
 
 
 export default function FriendCard(props) {
     
-    const [name, setName] = useState("empty");
-    const [course, setCourse] = useState("empty");    
-    const [userType, setUserType] = useState("empty");
+    const [name, setName] = useStateIfMounted("empty");
+    const [course, setCourse] = useStateIfMounted("empty");    
+    const [userType, setUserType] = useStateIfMounted("empty");
 
-
-    function userInfo(){
+    function updateInfo(){
         if (props.nome != null){
             setName(props.nome)    
         }
+
         if (props.course != null){
 
             setCourse(props.course)     
         }
+
         if (props.type != null){
             setUserType(props.type)     
         }
-        
-    };
-
-    userInfo();
+    }
 
     return(
-        <div>
+        <div onLoad={updateInfo}>
             <div className="friend-card">
-                    <div className="friend-content">
+                    <span className="friend-content">
                         <img className="friend-img" src="/static/media/minios.8f62a453.jpg" />
                         <h4 className="friend-name">{name}</h4>
                         <p className="friend-usertype">{course}</p>
                         <p className="friend-course"><b>{userType}</b> </p>    
-                    </div>
-
+                    </span>
             </div>
         </div>
     )
-}
+};
 
 /*
-
-                <div className="button-disconnect">
-                    <Link> <span>Desconectar</span> </Link>
-                </div>
+                    <div className="button-disconnect">
+                        <Link> <span>Desconectar</span> </Link>
+                    </div>
 */
