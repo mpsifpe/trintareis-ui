bc9b69d329eca6dce6bceb044bc39fb619bd1b51
Marcos Souza <marcos.souza@southsystem.com.br>
2023-02-27 21:45:25 -0300

    fix url

--- trintareis/src/config/api.js
+++ trintareis/src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://trintareis-hml.herokuapp.com'
+    baseURL: 'https://trintareis.herokuapp.com'
 })
 
 export default api;
