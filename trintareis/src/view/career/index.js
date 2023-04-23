0a2b894a49868fc12602b190dcdd85f7cdd483eb
Jonathan Martins <jcam1@discente.ifpe.edu.br>
2023-04-22 22:03:58 -0300

    career update

--- trintareis/src/view/career/index.js
+++ trintareis/src/view/career/index.js
@@ -22,16 +22,18 @@ export default function Career() {
         .then((response) => {
             setCardList(
                 response.data.map((course, index) => (
-                    <CourseCard 
-                        key={index}
-                        id={course.id}
-                        title={course.title}
-                        description={course.description}
-                        stateFirstLogin={location.state.firstLogin}
-                        stateProfilePhoto={location.state.profilePhoto} 
-                        stateCoverPhoto={location.state.coverPhoto} 
-                        stateUserData={location.state.userData}
-                    />
+                    <div className='course_div'>
+                        <CourseCard 
+                            key={index}
+                            id={course.id}
+                            title={course.title}
+                            description={course.description}
+                            stateFirstLogin={location.state.firstLogin}
+                            stateProfilePhoto={location.state.profilePhoto} 
+                            stateCoverPhoto={location.state.coverPhoto} 
+                            stateUserData={location.state.userData}
+                        />
+                    </div>
                 ))
             )
         })
@@ -50,12 +52,12 @@ export default function Career() {
         <div className="App">
             {redirect}
             <Header firstLogin={location.state.firstLogin} profilePhoto={location.state.profilePhoto} coverPhoto={location.state.coverPhoto} userData={location.state.userData} origin="career"/>
-                <div className="div__main_myfriends">
-                    <div className="div__title_myfriends">
+                <div className="div__main_career">
+                    <div className="div__title_career">
                         <span>Carreira</span>
                     </div>
                     
-                    <section className="section_friends_list" id="sec-bd5e">
+                    <section className="section_course_list" id="sec-bd5e">
                         {cardList}
                     </section>
                 </div>
